87371631f056e509e1a9b5b196172379311bb29f
zccz14 <zccz14@outlook.com>
2017-12-16 15:22:59 +0800

    Import Theme

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import React, {Component} from 'react';
 import Add from './Add'
 import Edit from './Edit'
 import Drawer from './Components/Drawer'
+import {createMuiTheme, MuiThemeProvider} from "material-ui/styles";
+import purple from 'material-ui/colors/purple';
+import red from 'material-ui/colors/red';
+import brown from "material-ui/colors/brown";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            ...red,
+            [500]: 'rgb(163, 46, 10)'
+        },
+        secondary: brown,
+        error: purple
+    }
+});
 
 class App extends Component {
     render() {
+        console.log(red);
         return (
-            <div className="full-height">
-                <Drawer/>
-
-                <Add/>
-                <Edit/>
-            </div>
+            <MuiThemeProvider theme={theme}>
+                <div className="full-height">
+                    <Drawer/>
+                    <Add/>
+                    <Edit/>
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
