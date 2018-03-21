6a613554a0526fb06298cb500c16d3ffd053285b
zccz14 <zccz14@outlook.com>
2018-03-22 04:57:09 +0800

    clear OrderPage

--- src/containers/OrderPage.js
+++ src/containers/OrderPage.js
@@ -4,7 +4,6 @@ import Button from 'material-ui/Button';
 import {Grid, Paper} from "material-ui";
 import OrderDetail from '../components/OrderDetailTable'
 import Menu from './Menu';
-import SnackBar from '../components/SnackBar'
 import {connect} from "react-redux";
 import {decOrderProduct, delOrderProduct, incOrderProduct, newOrder} from "../redux/order";
 import {Delete, Send} from "material-ui-icons";
@@ -43,143 +42,56 @@ const styles = theme => ({
     }
 });
 
-class OrderPage extends React.Component {
-    state = {
-        orderCreatedSnackBarOpen: false,
-    };
-
-    handleOrderAndSnackBar = () => {
-        this.setState({
-            orderCreatedSnackBarOpen: true,
-        });
-        this.props.dispatch({
-            type: "NEW_ORDER",
-            payload: {orders: this.props.order, total: caculateOrderToalCost(this.props.order)},
-        })
-    };
-
-
-    handleOrderCreatedSnackBaeClose = () => {
-        this.setState({
-            orderCreatedSnackBarOpen: false
-        })
-    };
-
-
-    handleOrderCanceled = () => {
-        this.props.dispatch({
-            type: "ORDER/CANCELED",
-            payload: ""
-        })
-    }
-    ;
-
-    render() {
-        const {classes, products, order, dispatch} = this.props;
-
-        const appendix = (
-            <div>
-                <Button variant="raised" color="primary" className={classes.action}
-                        onClick={() => {
-                            dispatch(newOrder());
-                            dispatch(createOrder({
-                                memberId: null,
-                                details: order,
-                                total: order.reduce((a, b) => a + b.amount * b.price, 0),
-                                note: ''
-                            }));
-                        }}>
-                    <Send/>
-                    确定下单
-                </Button>
-                <Button variant="raised" color="secondary" className={classes.action}
-                        onClick={() => dispatch(newOrder())}>
-                    <Delete/>
-                    取消订单
-                </Button>
-            </div>
-        );
-        return (
-            <div className={classes.root}>
-                <Grid container>
-                    <Grid item xs>
-                        <Paper>
-                            <Menu products={products}
-                                  onProductClick={product => dispatch(incOrderProduct(product.id))}/>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Paper>
-                            <OrderDetail
-                                data={order}
-                                onIncProduct={product => dispatch(incOrderProduct(product.id))}
-                                onDecProduct={product => dispatch(decOrderProduct(product.id))}
-                                onDelProduct={product => dispatch(delOrderProduct(product.id))}
-                                onDelete={() => dispatch(newOrder())}
-                            />
-                            {order.length > 0 ? appendix : null}
-                        </Paper>
-                    </Grid>
+const OrderPage = ({classes, products, order, dispatch}) => {
+    const appendix = (
+        <div>
+            <Button variant="raised" color="primary" className={classes.action}
+                    onClick={() => {
+                        dispatch(createOrder({
+                            memberId: null,
+                            details: order,
+                            total: order.reduce((a, b) => a + b.amount * b.price, 0),
+                            note: ''
+                        }));
+                    }}>
+                <Send/>
+                确定下单
+            </Button>
+            <Button variant="raised" color="secondary" className={classes.action}
+                    onClick={() => dispatch(newOrder())}>
+                <Delete/>
+                取消订单
+            </Button>
+        </div>
+    );
+    return (
+        <div className={classes.root}>
+            <Grid container>
+                <Grid item xs>
+                    <Paper>
+                        <Menu products={products}
+                              onProductClick={product => dispatch(incOrderProduct(product.id))}/>
+                    </Paper>
                 </Grid>
-                <SnackBar open={this.state.orderCreatedSnackBarOpen}
-                          onRequestClose={this.handleOrderCreatedSnackBaeClose} Note="订单已创建"/>
-            </div>
-        );
-    }
-}
+                <Grid item xs={8}>
+                    <Paper>
+                        <OrderDetail
+                            data={order}
+                            onIncProduct={product => dispatch(incOrderProduct(product.id))}
+                            onDecProduct={product => dispatch(decOrderProduct(product.id))}
+                            onDelProduct={product => dispatch(delOrderProduct(product.id))}
+                            onDelete={() => dispatch(newOrder())}
+                        />
+                        {order.length > 0 ? appendix : null}
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
 
-function caculateOrderToalCost(order) {
-    let total = 0;
-    for (let i = 0; i < order.length; i++) {
-        total = total + order[i].amount * order[i].price;
-    }
-    return total;
-}
-
-function addAmount(tempArray, order) {
-    return tempArray.map((a, i) => {
-        let ith = order.findIndex((v, ii) => v.name === a.name);
-        if (ith === -1) {
-            return {...a, amount: 0};
-        } else {
-            return {...a, amount: order[ith].amount};
-        }
-    })
-}
-
-function foldMenu(tempMenu, order) {
-    let temp = addAmount(tempMenu, order);
-    let array = [];
-    for (let i = 0; i < temp.length; i++) {
-        let ith = array.findIndex((ar, ii) => ar === temp[i].category);
-        if (ith === -1) {
-            array.push(temp[i].category)
-        }
-    }
-    let FoldMenu = [];
-    for (let j = 0; j < array.length; j++) {
-        FoldMenu.push({category: array[j], items: [],})
-    }
-    for (let k = 0; k < temp.length; k++) {
-        let indx = FoldMenu.findIndex((menu, i) =>
-            menu.category === temp[k].category
-        );
-
-        FoldMenu[indx].items.push(temp[k]);
-    }
-    return FoldMenu;
-}
-
-function ToArray(obj) {
-    let res = [];
-    for (let key in obj) {
-        res.push(obj[key]);
-    }
-    return res;
-}
 
 const selector = (state) => ({
-    menu: foldMenu(ToArray(state.repo.products.items), state.orders.curItems),
     order: Object.keys(state.order).map(id => ({...state.repo.products[id], amount: state.order[id]})),
     products: state.repo.products
 });
--- src/redux/index.js
+++ src/redux/index.js
@@ -4,7 +4,7 @@ import Products, {loadProducts} from './repo/products';
 import products from './products';
 import members from './members'
 import repo from './repo';
-import order from "./order";
+import order, {newOrder} from "./order";
 import saga from "redux-saga";
 import {put, takeEvery, takeLatest} from 'redux-saga/effects';
 import {CREATE_ORDER} from "./mutations";
@@ -46,9 +46,10 @@ sagas.run(function* () {
         let json = yield res.json();
         let data = json.data;
         yield put(createOrder(data.createOrder));
+        yield put(newOrder())
     });
     yield takeEvery(INIT, function* () {
-        const res = yield query('query { products(limit: 60) {id name price type} orders { id date status details { name price amount} member { id name } total } members { id name number }}');
+        const res = yield query('query { products(limit: 60) {id name price type} orders (limit: 200) { id date status details { name price amount} member { id name } total } members (limit: 2000) { id name number }}');
         const json = yield res.json();
         const data = json.data;
         yield put(loadProducts(data.products));
